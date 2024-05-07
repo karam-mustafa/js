7db9474ad77132fbdc67b309536f6687b748fa0f
karam mustafa <karam2mustafa@gmail.com>
2024-05-07 03:47:15 +0300

    wip

--- script.js
+++ script.js
@@ -1,136 +1,18 @@
-let firstName = "karam";
-let lastName = " mustafa";
-let age = 27;
-let city = "damascus";
-let country = "syria";
 
-let karamInfoObject = {
-  firstName: "karam", // accept all supported javascript data types
-  lastName: "mustafa",
-  age: "27",
-  city: "damascus",
-  country: "syria",
-};
+// أول شي بدي عرف مصفوفة اجباري لحتى خزن فيها البيانات يلي عم ضيفها على الجدول - كل ما ببعت سطر جديد للجدول بضيفه ككائن على المصفوفة
+const data = [];
 
-let aliInfoObject = {
-  firstName: "ali", // accept all supported javascript data types
-  lastName: "mohammed",
-  age: "21",
-  city: "latakia",
-  country: "syria",
-};
 
-let nouraInfoObject = {
-  firstName: "noura", // accept all supported javascript data types
-  lastName: "mahmoud",
-  age: "24",
-  city: "latakia",
-  country: "syria",
-};
 
-// let karamInfo = firstName + lastName + " and his age: " + age + " he lives in: " + city + " in: " + country;
-// let karamInfo = `${firstName} ${lastName} and his age: ${age} and he lives in: ${city} in: ${country}`;
-let displayKaramInfo = `${karamInfoObject.firstName} ${karamInfoObject.lastName} and his age: ${karamInfoObject.age} and he lives in: ${karamInfoObject.city} in: ${karamInfoObject.country}`;
-let displayAliInfo = `${aliInfoObject.firstName} ${aliInfoObject.lastName} and his age: ${aliInfoObject.age} and he lives in: ${aliInfoObject.city} in: ${aliInfoObject.country}`;
-let displayNouraInfo = `${nouraInfoObject.firstName} ${nouraInfoObject.lastName} and his age: ${nouraInfoObject.age} and he lives in: ${nouraInfoObject.city} in: ${nouraInfoObject.country}`;
+//////////////////////////////////////////////// الاضافة ////////////////////////////////////////////////  
 
-// console.log(displayKaramInfo);
-// console.log('===========================');
-// console.log(displayAliInfo);
-// console.log('===========================');
-// console.log(displayNouraInfo);
-
-let students = [
-  "ali0", // 0
-  "ali1", // 1
-  "ali2", // 2
-  "ali3", // 3
-  "ali4", // 4
-  "ali5", // 5
-  "ali6", // 6
-  "ali7", // 7
-  "ali8", // 8
-  "ali9", // 9
-];
-
-let text = "";
-
-// for loop
-// for (let index = 0; index < students.length; index++) {
-// text = text + " " + students[index];
-// document.write(`${students[index]} \n`);
-// }
-
-// forEach
-
-// while
-// let index = 0;
-// while (index < students.length) {
-//   document.write(`${students[index]} \n`);
-//   index++;
-// }
-
-// function printArrayValues(ahmed, ali) {
-
-//   students.forEach(function (item) {
-//     console.log(item);
-//   });
-
-//   console.log(ahmed + ali);
-// }
-
-// printArrayValues(30, 20);
-
-// let programmingLanguages = [
-//   'C++',
-//   'C',
-//   'JS',
-//   'PHP',
-//   'JAVA',
-// ];
-
-// function printLanguages(lang) {
-//   // create the element
-//   let createdElement = document.createElement('div')
-//   // give the element new text
-//   createdElement.innerText = `This is: ${lang}`;
-//   // select the body
-//   let body = document.querySelector('body');
-//   // give the body a new child
-//   body.appendChild(createdElement)
-// }
-
-// programmingLanguages.forEach((item) => {
-
-//   // for each item like C++, C, Js ..etc
-//   // run this function
-//   printLanguages(item)
-// })
-
-// arrow function
-// const print = () => {
-//   console.log(1);
-// }
-
-// print()
-
-// const tr = document.createElement('tr');
-// const th = document.createElement('th');
-// th.innerText = '3';
-// const td1 = document.createElement('td');
-// td1.innerHTML = 'karam';
-// const td2 = document.createElement('td');
-// td2.innerHTML = 'mustafa';
-// const td3 = document.createElement('td');
-// td3.innerHTML = 'karam';
-
-// tr.appendChild(th)
-// tr.appendChild(td1)
-// tr.appendChild(td2)
-// tr.appendChild(td3)
-
+// Add a new row هي الدالة الخاصة باضافة سطر جديد للجدول وهي بتتنفذ لما نكبس على زر 
 const addRowToTable = (name, last, handle, elementIndex) => {
+
+  // عم اعملي سيليكت على الجدول
   const tableBody = document.querySelector(".table tbody");
+  
+  //  عم حط بقلب هذا الجدول السطر الجديد بكل عناصره
   tableBody.insertAdjacentHTML(
     "beforeend",
     `
@@ -140,48 +22,145 @@ const addRowToTable = (name, last, handle, elementIndex) => {
               <td>${last}</td>
               <td>${handle}</td>
               <td>      
+
+              
               <button onclick="deleteRow(event)" class="btn btn-danger delete-button-${elementIndex}">Delete row</button>
+             
+             
+              بسبب انه بدي اعرف لما اضغط على زر التحديث عند اي عنصر بالجدول ضغطت elementIndex  هون حطيت 
+              <button data-index=${elementIndex} onclick="updateRow(event)" class="btn btn-warning delete-button-${elementIndex}">Update</button>
+             
               </td>
           </tr>
   `
   );
+
+  //  وهذا الشي يساعدني جيب البيانات بسرعة بدون ما روح دور عليها بالجدولHTML هون ضفت السطر ككائن على المصفوفة بعد ما شفته على الجدول ب 
+  data.push({
+    name: name,
+    last: last,
+    handle: handle,
+  });
 };
 
 
+//  تبعه صفرIndex هون عرفت اول عنصر على اساس هو العنصر يلي   
 let elementIndex = 0;
 
+//  هون بدي جيب الزر تبع الاضافة
 const newRowButton = document.querySelector(".add-new-button");
-// option1
+
+
+// ضفتله مراقب للاحداث مشان لما اضغط عليه نفذ اوامر معينة
 newRowButton.addEventListener("click", () => {
-  
-  // get the input values
+
+  // هون جبنا القيم المكتوبة بحقول الادخال
   const nameInput = document.querySelector(".name");
   const lastInput = document.querySelector(".last");
   const handleInput = document.querySelector(".handle");
-  
-  // add the new row
-  addRowToTable(nameInput.value, lastInput.value, handleInput.value, elementIndex);
 
-  // option2
-  // newRowButton.addEventListener("click", addRowToTable);
+  // هون ذغلت دالة الاضافة مشان يضيف السطر على الجدول والمصفوفة وعطيته القيم من حقول الادخال
+  addRowToTable(
+    nameInput.value,
+    lastInput.value,
+    handleInput.value,
+    elementIndex
+  );
 
-  // get the delete button
-  const deleteButton = document.querySelector(`.delete-button-${elementIndex}`);
-
-  // add delete button on user click
-  deleteButton.addEventListener("click", (event) => {
-    event.target.parentElement.parentElement.remove();
-  });
-
+  // هون زودت المتغير بواحد مشان السطر يلي بدي ضيفه بعده ينزل برقم جديد وزايد عن يلي قبلو واحد
   elementIndex += 1;
 
+  // هون رجع فضيت القيم يلي موجودين بحقول الادخال مشان لما بدي ضيف سطر جديد تكون الحقول فاضية وما اتعذب بمسح القيم وكتابة قيم جديدة
+  nameInput.value = "";
+  lastInput.value = "";
+  handleInput.value = "";
 
-  nameInput.value = '';
-  lastInput.value = '';
-  handleInput.value = '';
 
 });
+//////////////////////////////////////////////// انتهت الاضافة ////////////////////////////////////////////////  
 
+
+//////////////////////////////////////////////// الحذف ////////////////////////////////////////////////  
+// هون ضفت دالة الحذف
 const deleteRow = (event) => {
+  //بعدين احذفه TR  هون عم خبره انه بدالة الحذف جي الزر وبعدين انتقل لمستوى الأب بعدين لمستوى الأب مرة ثانية مشان كون وصلت لل 
   event.target.parentElement.parentElement.remove();
 };
+//////////////////////////////////////////////// انتهى الحذف ////////////////////////////////////////////////  
+
+
+
+//////////////////////////////////////////////// التعديل ////////////////////////////////////////////////  
+// اول شي عم جيب زر الحفظ
+const saveButton = document.querySelector(".save-after-update");
+
+// للعنصر يلي ضغطتت زر التعديل عليه  Index ثاني شي عم اعمل متغير جديد يفيدني بمعرفة شو  
+let lastIndex = 0;
+
+const updateRow = (event) => {
+
+  // data-index يلي خزته سابقاً بالزر بال  Index هون جبت الرقم من ال 
+  const index = event.target.dataset.index;
+
+  // اسندت القيمة للمتغير يلي عرفته فوق
+  lastIndex = index;
+
+
+  // جبت البيانات من العنصر يلي انحفظ بالمصفوفة اول شي عند الاضافة مشان اعرف شو قيم السطر
+  const todoItem = data[index];
+
+  // هون كشفنا الستار عن زر الحفظ
+  saveButton.classList.remove("d-none");
+
+  // هون عم ارجع اعمل سيليكت لحقول الادخال
+  const nameInput = document.querySelector(".name");
+  const lastInput = document.querySelector(".last");
+  const handleInput = document.querySelector(".handle");
+
+  // هون رجعت حطيت القيم يلي اخذتها من الكائن يلي جبته من المصفوفة بحقول الادخال
+  nameInput.value = todoItem.name;
+  lastInput.value = todoItem.last;
+  handleInput.value = todoItem.handle;
+};
+
+// هلأ هون عم ضيف مراقب لزر الحفظ مشان بس ينكبس ينفذ مجموعة اوامر
+saveButton.addEventListener("click", () => {
+
+  //  اول شي عم ارجع جيب حقول الادخال
+  const nameInput = document.querySelector(".name");
+  const lastInput = document.querySelector(".last");
+  const handleInput = document.querySelector(".handle");
+
+  // ثاني شي عم جيب الجدول
+  const tbody = document.querySelector("tbody");
+
+  // 98 يلي خزنته بالمتغير بالسطر  Index يلي بدي عدله حسب رقم  TR ثالث شي عم جيب ال  
+  // بعدين عم اعطيه المحتوى الجديد على حسب شو مكتوب بحقول الادخال
+  tbody.children[lastIndex].innerHTML = `
+        <th scope="row">${lastIndex}</th>
+        <td>${nameInput.value}</td>
+        <td>${lastInput.value}</td>
+        <td>${handleInput.value}</td>
+        <td>      
+            <button onclick="deleteRow(event)" class="btn btn-danger delete-button-${lastIndex}">Delete row</button>
+            <button data-index=${lastIndex} onclick="updateRow(event)" class="btn btn-warning delete-button-${lastIndex}">Update</button>
+        </td>
+  `;
+
+  //  بعدين عم ارجع حدث العنصر نفسه بالمصفوفة 
+  // وبالمصفوفة HTML  دوما بدي حدث بمكانين بالجدول بال 
+  data[lastIndex] = {
+    name: nameInput.value,
+    last: lastInput.value,
+    handle: handleInput.value,
+  };
+
+  // هون عم ارجع اخفي زر الحفظ
+  saveButton.classList.add("d-none");
+
+  // واخر شي عم نرجع نفضي البيانات من حقول الادخال
+  nameInput.value = "";
+  lastInput.value = "";
+  handleInput.value = "";
+});
+//////////////////////////////////////////////// انتهى التعديل ////////////////////////////////////////////////  
